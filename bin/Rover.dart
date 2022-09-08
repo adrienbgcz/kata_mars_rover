1ebdf07b3a59aba51e526f007f92c7f707fe66a7
Adrien Bogacz <ad.bogacz@gmail.com>
2022-09-08 14:22:41 +0200

    refacto 1

--- bin/Rover.dart
+++ bin/Rover.dart
@@ -11,96 +11,60 @@ class Rover {
 
   Rover(this.name, this.grille, {this.posX = 0, this.posY = 0, this.watchingDirection = constantDirection.NORTH});
 
+  void checkDirection(direction) {
+    if(watchingDirection == direction) {
+      print("J'avance");
+    } else {
+      turn(direction);
+      print("Maintenant j'avance vers $watchingDirection");
+    }
+  }
+
+
   void move({int x = 0, int y = 0}) {
     print("Actuellement je regarde vers $watchingDirection");
     if(posX + x > grille.xMax ) { // avance vers l'EST
-      if(watchingDirection == constantDirection.EAST) {
-        print("J'avance");
-      } else {
-        turn(constantDirection.EAST);
-      }
+      checkDirection(constantDirection.EAST);
       posX = ((posX + x) - grille.xMax) - 1;
     }
     else if (posX + x < 0) {
-      if(watchingDirection == constantDirection.WEST) {
-        print("J'avance");
-      } else {
-        turn(constantDirection.WEST);
-        print("Maintenant j'avance vers $watchingDirection");
-      }
+      checkDirection(constantDirection.WEST);
     posX = ((posX + x) + grille.xMax) + 1;
     }
     else {
       if(posX + x > posX) {
-        if(watchingDirection == constantDirection.EAST) {
-          print("J'avance");
-        } else {
-          turn(constantDirection.EAST);
-          print("Maintenant j'avance vers $watchingDirection");
-        }
+        checkDirection(constantDirection.EAST);
       }
       else if(x == 0) {
        print("Je ne bouge pas");
       }
       else {
-        if(watchingDirection == constantDirection.WEST) {
-          print("J'avance");
-        } else {
-          turn(constantDirection.WEST);
-          print("Maintenant j'avance vers $watchingDirection");
-        }
+        checkDirection(constantDirection.WEST);
       }
       posX += x;
     }
 
 
     if(posY + y > grille.yMax ) { // avance vers le NORD
-      if(watchingDirection == constantDirection.NORTH) {
-        print("J'avance");
-      } else {
-        turn(constantDirection.NORTH);
-      }
+      checkDirection(constantDirection.NORTH);
       posY = ((posY + y) - grille.yMax) - 1;
     }
     else if (posY + y < 0) {
-      if(watchingDirection == constantDirection.SOUTH) {
-        print("J'avance");
-      } else {
-        turn(constantDirection.SOUTH);
-        print("Maintenant j'avance vers $watchingDirection");
-      }
+      checkDirection(constantDirection.SOUTH);
       posY = ((posY + y) + grille.yMax) + 1;
-    }
-    else if(y == 0) {
-      print("Je ne bouge pas");
     }
     else {
       if(posY + x > posY) {
-        if(watchingDirection == constantDirection.NORTH) {
-          print("J'avance");
-        } else {
-          turn(constantDirection.NORTH);
-          print("Maintenant j'avance vers $watchingDirection");
-        }
+        checkDirection(constantDirection.NORTH);
+      }
+      else if(y == 0) {
+        print("Je ne bouge pas");
       }
       else {
-        if(watchingDirection == constantDirection.SOUTH) {
-          print("J'avance");
-        } else {
-          turn(constantDirection.SOUTH);
-          print("Maintenant j'avance vers $watchingDirection");
-        }
+        checkDirection(constantDirection.SOUTH);
       }
       posY += y;
     }
-
-    /*if(posY + y > grille.yMax ) {
-      posY = ((posY + y) - grille.yMax) - 1;
-    } else if (posY + y < 0) {
-      posY = ((posY + y) + grille.yMax) + 1;
-    } else {
-      posY += y;
-    }*/
   }
 
   void turn(direction) {
